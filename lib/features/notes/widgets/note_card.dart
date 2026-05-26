b36519fe2f49dda677b2b74e0738f0823f8adbc6
Venky888 <vennky11111@gmail.com>
2026-05-26 15:58:10 +0530

    Modified note_card

--- lib/features/notes/widgets/note_card.dart
+++ lib/features/notes/widgets/note_card.dart
@@ -116,7 +116,7 @@ class NoteCard extends StatelessWidget {
                   const SizedBox(height: 8),
                   _actionButton(
                     icon: Icons.delete_outline,
-                    color: Colors.red,
+                    color: Colors.green,
                     onTap: onDelete,
                   ),
                 ],
